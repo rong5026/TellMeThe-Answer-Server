c4e31b4860606eed630d9c14d29b385960f7808e
조준희 <iopp3423@gmail.com>
2023-10-06 14:25:38 +0900

    [UPDATE] Report클래스 코드 업데이트

--- src/main/java/com/capstone/answer/domain/report/Report.java
+++ src/main/java/com/capstone/answer/domain/report/Report.java
@@ -1,4 +1,42 @@
 package com.capstone.answer.domain.report;
 
-public class Report {
+
+import com.capstone.answer.domain.BaseTimeEntity;
+import com.capstone.answer.domain.image.Image;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "member")
+@Getter
+public class Report extends BaseTimeEntity {
+
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name = "member_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 40)
+    private String title;
+
+    @Column(nullable = false, length = 40)
+    private String location;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false, length = 40)
+    private String plane;
+
+    @Column(nullable = false, length = 40)
+    private String disease;
+
+    @OneToMany(mappedBy = "writer", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<Image> imageLink = new ArrayList<>();
 }
