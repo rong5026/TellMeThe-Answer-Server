3377bbaa6eae1a23e4ca09e9d884d5ca470dfd99
rong5026 <rong5026@naver.com>
2023-11-05 18:21:07 +0900

    [ADD] BaseReportDTO추가

--- src/main/java/com/capstone/answer/domain/report/dto/BaseReportDto.java
+++ src/main/java/com/capstone/answer/domain/report/dto/BaseReportDto.java
@@ -2,18 +2,14 @@ package com.capstone.answer.domain.report.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
-public class ReportListDto {
-    
-    private Long reportId;
+public class BaseReportDto {
     private String title;
     private String content;
     private float latitude;
     private float longitude;
     private String plant;
     private String disease;
-    private Long memberId;
-//   이미지 리스트
 }
-
--- src/main/java/com/capstone/answer/domain/report/dto/ReportAddDto.java
+++ src/main/java/com/capstone/answer/domain/report/dto/ReportAddDto.java
@@ -4,14 +4,8 @@ import com.capstone.answer.domain.report.entity.Report;
 import lombok.Getter;
 
 @Getter
-public class ReportAddDto {
+public class ReportAddDto extends BaseReportDto{
 
-    private String title;
-    private String content;
-    private float latitude;
-    private float longitude;
-    private String plant;
-    private String disease;
     private String email; // 사용자 이메일
 
 }
